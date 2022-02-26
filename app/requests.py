e6083f2d24dea8b129adb7f39639592fdadf19de
melodytowett <melodytowett99@gmail.com>
2022-02-26 14:36:39 +0300

    add funtion to process headlines results

--- app/requests.py
+++ app/requests.py
@@ -1,4 +1,5 @@
 from concurrent.futures import process
+from turtle import title
 from unicodedata import category
 import urllib.request,json
 
@@ -20,6 +21,42 @@ def configure_request(app):
     headline_url = app.confg['HEADLINE_URL']
 
 
+def get_headlines():
+    '''
+    funtcion that gets response from json url request
+    '''
+    get_headlines_url = headline_url.format(api_key)
+    with urllib.request.urlopen(get_headlines_url) as url:
+        get_headlines_data = url.read()
+        get_headlines_response = json.loads(get_headlines_data)
+        headlines_results = None
+        if get_headlines_response['articles']:
+            headlines_results_list = get_headlines_response['articles']
+            headlines_results = process_headlines_results(headlines_results_list)
+
+        return headlines_results
+
+def process_headlines_results(headline_list):
+    '''
+    funtion to process headlines results
+    '''
+    headlines_results = []
+    for headline_item in headline_list:
+        image = headline_item.get('urlToImage')
+        title = headline_item.get('title')
+        author = headline_item.get('author')
+        description = headline_item.get('description')
+        time = headline_item.get('publishedAt')
+        url = headline_item.get('url')
+
+        if image:
+            headline_object = Article(image,title,author,description,time,url)
+            headlines_results.append(headline_object)
+
+    return headlines_results
+
+
+
 def get_category(category):
     '''
     Fuction that gets the json respone to the url request
@@ -33,11 +70,7 @@ def get_category(category):
 
         if get_category_response['results']:
             category_results_list = get_category_response['results']
-            category_results = process_results(category_results_list)
+            category_results = process_headlines_results(category_results_list)
 
     return category_results
 
-def process_results(category_list):
-    '''
-    Function to process the news category 
-    '''
