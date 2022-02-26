aac08b236589cc89a28d22659c8c7d23d991260d
melodytowett <melodytowett99@gmail.com>
2022-02-26 15:47:07 +0300

    register blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -10,4 +10,8 @@ def create_app(config_name):
     app.config.from_object(Config_options[config_name])
 
     bootstrap.init_app(app)
+
+    #register the blueprint
+    from .main import main as main_blueprint
+    app .register_blueprint(main_blueprint)
     return app
