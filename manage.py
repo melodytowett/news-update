6cbfd92fff0d0d92eb8d68d48725aa4e7a430ae3
melodytowett <melodytowett99@gmail.com>
2022-02-26 15:57:29 +0300

    register blueprint

--- manage.py
+++ manage.py
@@ -1,9 +1,9 @@
 from logging import Manager
-from xmlrpc.client import Server
 from app import create_app
+from flask_script import Manager,Server
 app = create_app('development')
 manager = Manager(app)
-#manager.add_commad('server', Server)
+manager.add_commad('server', Server)
 
 if __name__ == '__main__':
     manager.run()
