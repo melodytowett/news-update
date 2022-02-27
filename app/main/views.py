1d6d06c62c2360c3d961ee27a0e0c20f84273345
melodytowett <melodytowett99@gmail.com>
2022-02-27 20:56:16 +0300

    add search function

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,3 @@
-from turtle import title
 from ..requests import get_headlines,get_category,get_source,get_source_articles, search_topic
 from . import main
 from flask import render_template,request,redirect,url_for
@@ -9,13 +8,15 @@ def index():
     View root page function that returns the index page
     '''
     headlines = get_headlines()
-    categories = get_category("sources")
+    categories = get_category("business")
     sources = get_source()
+    title = 'Home-News Update Website'
     search_topic = request.args.get('q')
+
     if search_topic:
         return(url_for('.search',topic_name = search_topic))
     else:
-        return render_template('index.html',headlines= headlines,categories = categories,sources = sources)
+        return render_template('index.html',headlines= headlines,categories = categories,title=title,sources = sources)
 
 @main.route('/categories/<category>')
 def category(category):
@@ -32,6 +33,7 @@ def article(id):
     sources = get_source()
     category = get_category('health')
     return render_template('article.html',article = article,sources = sources, category = category)
+    
 @main.route('/search/<topic_name>')
 def search(topic_name):
     ''' 
@@ -40,5 +42,4 @@ def search(topic_name):
     topic_name_list = topic_name.split(" ")
     topic_name_format = "+".join(topic_name_list)
     searched_topics = search_topic(topic_name_format)
-    title = f'search resuls for{topic_name}'
     return render_template('search.html',searched_topic = searched_topics)
