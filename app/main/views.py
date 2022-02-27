b6a4c493ba819c1e80336174a66ad974bdd4198e
melodytowett <melodytowett99@gmail.com>
2022-02-27 15:10:40 +0300

    add file templates to display articles

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,3 @@
-from turtle import title
-from app.models import Source
 from ..requests import get_headlines,get_category,get_source,get_source_articles
 from . import main
 from flask import render_template,request,redirect,url_for
@@ -10,11 +8,9 @@ def index():
     View root page function that returns the index page
     '''
     headlines = get_headlines()
-    categories = get_category("sources")
-    sources = get_source()
-    articles = get_source_articles('articles')
-    title = 'Home- Welcome to News update'
-    return render_template('index.html',headlines= headlines,title=title,sources=sources,article = articles,category = categories)
+    # categories = get_category("sources")
+    # sources = get_source()
+    return render_template('index.html',headlines= headlines)
 
 @main.route('/categories/<category>')
 def category(category):
@@ -28,5 +24,11 @@ def category(category):
 @main.route('/article/<id>')
 def article(id):
     article = get_source_articles(id)
-    title = f'{article.title}'
-    return render_template('article.html',article = article,title = title)
+    sources = get_source()
+    category = get_category('health')
+    return render_template('article.html',article = article,sources = sources, category = category)
+@main.route('/search/<topic_name>')
+def search(topic_name):
+    ''' 
+    Function to display search results
+    '''
